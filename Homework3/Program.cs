dc0a4b37308db5128edeb3779980c9b5b54b7220
DarStar <merlanor@gmail.com>
2022-09-08 13:44:34 +0300

    add task 19

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("Enter 5-digit number ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+while (1000 > num || num > 99999)
+        Console.WriteLine("Wroooong, do it again!");
+
+if (Convert.ToString(num)[0] == Convert.ToString(num)[4] && Convert.ToString(num)[1] == Convert.ToString(num)[3]);
+    Console.WriteLine("It is a palindrome");
+
+else
+    Console.WriteLine("No, it's not a palindrome");
