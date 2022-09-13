42a85a582a93a5e317e5f9a5a4a0012e09108c6a
DarStar <merlanor@gmail.com>
2022-09-13 15:01:37 +0300

    add task 27 and 496

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -31,29 +31,80 @@
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-// 452 -> 11
-
-// 82 -> 10
-
-// 9012 -> 12
-
 // Console.WriteLine("Enter number with more than 2-digits: ");
 // int num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
 
 // if (num < 10)
 // Console.WriteLine("Number doesn't have 2 digits");
 
-// Console
-
+// while (num > 0)
+// {
+//     sum += num % 10;
+//     num /= 10;
+// }
+// Console.WriteLine(sum);
 
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-int[] array = new int[8];
+// int[] array = new int[8];
 
-for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(0, 100);
+// for (int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(0, 100);
     
- Console.WriteLine("[" + string.Join(", ", array) + "]");
+//  Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+//Задача 496. Сбор черники:
+
+// Всего на грядке растет N кустов. 
+//Эти кусты обладают разной урожайностью, поэтому ко времени сбора на них выросло различное число ягод – на i-ом кусте выросло ai ягод.
+//Собирающий модуль за один заход, находясь непосредственно перед некоторым кустом, собирает ягоды с этого куста и с двух соседних с ним
+//Напишите программу для нахождения максимального числа ягод
+//которое может собрать за один заход собирающий модуль
+//находясь перед некоторым кустом заданной во входном файле грядки.
+//целое число N (3 ≤ N ≤ 1000) – количество кустов черники.
+//Вторая строка содержит N целых положительных чисел a1, a2, ..., 
+//aN – число ягод черники, растущее на соответствующем кусте. Все ai не превосходят 1000.
+
+// Console.WriteLine("Сколько кустов: ");
+// int bush = Convert.ToInt32(Console.ReadLine());
+
+
+// if(bush < 3 || bush > 999)
+// Console.WriteLine("Неверное число кустов");
+
+// else
+// {
+// int[] berries = new int[bush];
+
+// for (int i = 0; i < berries.Length; i++)
+//      berries[i] = new Random().Next(0, 100);
+    
+//   Console.WriteLine("Ягод на кустах: [" + string.Join(", ", berries) + "]");
+
+
+// int[] max_berries = new int[bush];
+// for (int i = 1; i < berries.Length - 1; i++)
+// {
+//     max_berries[i - 1] = berries[i - 1] + berries[i] + berries[i + 1];
+// }
+// max_berries[max_berries.Length - 2] = berries[berries.Length - 1] + berries[0] + berries[1];
+// max_berries[max_berries.Length - 1] = berries[0] + berries[berries.Length - 1] + berries[berries.Length - 2];
+// Console.WriteLine("Максимальное с трех кустов: [" + string.Join(", ", max_berries) + "]");
+
+// int max = max_berries[0];
+// for (int i = 1; i < max_berries.Length; i++)
+// {
+//     if (max < max_berries[i])
+//         max = max_berries[i];
+// }
+// Console.WriteLine("Максимаьное количество ягод: " + max);
+// }
+
+
+
+
+
